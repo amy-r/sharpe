f1cb8cbd394794c8abbc648b94a982613e6d4e41
Amy Russell <russell.amyh@gmail.com>
2018-03-18 13:01:32 -0600

    sharpe function test and fetchApi test passing

--- src/helper.js
+++ src/helper.js
@@ -3,5 +3,18 @@ var math = require('mathjs');
 export const sharpeRatio = (arrOfRtrns) => {
   const sharpe = (math.mean(arrOfRtrns) / math.std(arrOfRtrns))
   return sharpe
-  //sharpe = math.mean(return) / math.std(return)
+}
+
+export const fetchApi = async (url) => {
+  try {
+    const response = await fetch(url);
+
+    if (response.status <= 200) {
+      return await response.json()
+    } else {
+      throw new Error('Bad status code')
+    }  
+  }  catch(err) {
+    throw new Error('Could not get data')
+  }
 }
--- src/helper.test.js
+++ src/helper.test.js
@@ -1,22 +1,54 @@
-import { sharpeRatio } from './helper.js';
+import { sharpeRatio, fetchApi } from './helper.js';
+import {mockDay, mockWeek,mockMonth, mockYear} from './mockData.js'
 var math = require('mathjs');
 
 describe('helper', () => {
-
-  describe('sharpeRatio', () => {
-    const mockArr = [1, 2, 3, 4, 5];
-    const avg = 3;
-    const standardDev = 1.5811388300841898;
+  describe('fetchApi', () => {
     
-    it('should take in an array of numbers', () => {
-      const sharpeRatio = jest.fn();
-      sharpeRatio(mockArr);
-      expect(sharpeRatio).toHaveBeenCalledWith(mockArr);
+    let url
+
+    beforeAll(() => {
+      url = 'https://api.nomics.com/v0/sparkline'
+
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+          status: 200,
+          json: () => Promise.resolve({ 
+            day: mockDay,
+            week: mockWeek,
+            month: mockMonth,
+            year: mockYear 
+          })
+        })
+      )
     })
 
-    it('should return the sharpe ratio', () => {
-      // console.log(math.std(mockArr))
-      expect(sharpeRatio(mockArr)).toEqual(1.8973665961010275)
+    it('calls fetch with the correct params', () => {
+      const expectParams = ['https://api.nomics.com/v0/sparkline']
+      expect(window.fetch).not.toHaveBeenCalled();
+
+      fetchApi(url)
+      expect(window.fetch).toHaveBeenCalledWith(...expectParams)
     })
+
+    it('returns a response object', async () => {
+      const response = await fetchApi(url)
+      const expected = {  
+        day: mockDay,          
+        week: mockWeek,
+        month: mockMonth,
+        year: mockYear
+      }
+      expect(response).toEqual(expected)
+    })
+
+    it('should throw an error if the response is not okay', () => {
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+          status: 500,   
+        })
+      )
+
+      expect(fetchApi(url)).rejects.toEqual(Error('Could not get data'))
+    })
+
   })
 })
