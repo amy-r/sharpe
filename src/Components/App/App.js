aa8d1090988dafedae11d80274029aa71ef80c96
Amy Russell <russell.amyh@gmail.com>
2018-03-19 16:31:19 -0600

    setting currency returns into state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,11 +4,17 @@ import './App.css';
 import * as helper from '../../helper/helper.js'
 
 class App extends Component {
-
+  constructor() {
+    super();
+    this.state = {
+      currencyReturns: []
+    }
+  }
   componentDidMount = async () => {
     const fetched = await helper.fetchApi('https://api.nomics.com/v0/sparkline')
     console.log(fetched)
-    helper.getData(fetched)
+    const currencyReturns = helper.getData(fetched)
+    this.setState({ currencyReturns })
   }
 
   render() {
